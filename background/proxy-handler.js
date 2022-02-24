f39b2e49a4a97d76721ae29bfd252c0c809a830a
Ray Fung <posixfung@163.com>
2022-02-24 18:45:21 +0800

    Added window specific proxy

--- background/proxy-handler.js
+++ background/proxy-handler.js
@@ -3,15 +3,9 @@
 let bypassHosts = ["localhost", "[::1]"];
 let proxyHost = "127.0.0.1";
 let proxyPort = 0;
-let proxyId = 0;
 
-browser.runtime.onInstalled.addListener(details => {
-  browser.storage.local.set({
-    proxyHost: proxyHost,
-    proxyPort: proxyPort,
-    proxyId: proxyId
-  });
-});
+let globalProxyId = 0;
+let windowProxyId = {};
 
 browser.storage.local.get(data => {
   if (typeof data.proxyHost !== 'undefined') {
@@ -21,12 +15,6 @@ browser.storage.local.get(data => {
   if (typeof data.proxyPort !== 'undefined') {
     proxyPort = data.proxyPort;
   }
-
-  if (typeof data.proxyId !== 'undefined') {
-    proxyId = data.proxyId;
-  }
-
-  updateIcon();
 });
 
 browser.storage.onChanged.addListener(changeData => {
@@ -37,12 +25,6 @@ browser.storage.onChanged.addListener(changeData => {
   if (changeData.proxyPort) {
     proxyPort = changeData.proxyPort.newValue;
   }
-
-  if (changeData.proxyId) {
-    proxyId = changeData.proxyId.newValue;
-  }
-
-  updateIcon();
 });
 
 // Managed the proxy
@@ -51,17 +33,38 @@ browser.storage.onChanged.addListener(changeData => {
 browser.proxy.onRequest.addListener(handleProxyRequest, {urls: ["<all_urls>"]});
 
 // On the request to open a webpage
-function handleProxyRequest(requestInfo) {
+async function handleProxyRequest(requestInfo) {
+  if (isWindowProxyEmpty() || requestInfo.tabId == -1) {
+    // Use global config
+    return getProxyById(globalProxyId, requestInfo);
+  }
+
+  // Try getting window specific config
+  let tabInfo = await browser.tabs.get(requestInfo.tabId);
+  return getProxyById(getWindowFinalProxyId(tabInfo.windowId), requestInfo);
+}
+
+function isWindowProxyEmpty() {
+  for (var x in windowProxyId) { return false; }
+  return true;
+}
+
+function getWindowFinalProxyId(windowId) {
+  let proxyId = windowProxyId[windowId];
+  return proxyId === undefined ? globalProxyId : proxyId;
+}
+
+function getProxyById(proxyId, requestInfo) {
   if (proxyId == 0) {
-    return {type: "direct"};
+    return { type: "direct" };
   }
   else {
     const url = new URL(requestInfo.url);
     if (url.hostname.startsWith("127.") || bypassHosts.indexOf(url.hostname) != -1) {
-      return {type: "direct"};
+      return { type: "direct" };
     }
 
-    return {type: "socks", host: proxyHost, port: proxyPort, proxyDNS: true};
+    return { type: "socks", host: proxyHost, port: proxyPort, proxyDNS: true };
   }
 }
 
@@ -70,28 +73,52 @@ browser.proxy.onError.addListener(error => {
   console.error(`Proxy error: ${error.message}`);
 });
 
-function updateIcon() {
-  if (proxyId == 0) {
-    browser.browserAction.setIcon({path: "icons/direct.png"});
-    //browser.browserAction.setBadgeText({text: "0"});
-    //browser.browserAction.setBadgeBackgroundColor({'color': 'yellow'});
+async function updateIcon() {
+  browser.browserAction.setIcon({ path: getProxyIconPath(globalProxyId) });
+
+  let winInfos = await browser.windows.getAll();
+  for (var info of winInfos) {
+    let proxyId = getWindowFinalProxyId(info.id);
+    browser.browserAction.setIcon({ path: getProxyIconPath(proxyId), windowId: info.id });
   }
-  else {
-    browser.browserAction.setIcon({path: "icons/main.png"});
-    //browser.browserAction.setBadgeText({text: "1"});
-    //browser.browserAction.setBadgeBackgroundColor({'color': 'green'});
-  }
+}
+
+function getProxyIconPath(proxyId) {
+  return proxyId == 0 ? "icons/direct.png" : "icons/main.png";
+}
+
+function getProxyState(windowId) {
+  const wpid = windowProxyId[windowId];
+  return Promise.resolve({ global: globalProxyId, window: wpid });
 }
 
 function handleMessage(msg) {
   switch (msg.command) {
-    case "direct":
-      proxyId = 0;
+    case "global_direct":
+      globalProxyId = 0;
+      windowProxyId = {};
       break;
 
-    case "proxy":
-      proxyId = 1;
+    case "global_proxy":
+      globalProxyId = 1;
+      windowProxyId = {};
       break;
+
+    case "window_direct":
+      windowProxyId[msg.windowId] = 0;
+      break;
+
+    case "window_proxy":
+      windowProxyId[msg.windowId] = 1;
+      break;
+
+    case "get_proxy_state":
+      return getProxyState(msg.windowId);
+
+    case "log_proxy":
+      console.log("Global proxy: %d\nWindow proxy: %o", globalProxyId,
+        JSON.parse(JSON.stringify(windowProxyId)));
+      return;
   }
 
   updateIcon();
@@ -99,4 +126,3 @@ function handleMessage(msg) {
 
 updateIcon();
 browser.runtime.onMessage.addListener(handleMessage);
-
--- popup/choose_proxy.js
+++ popup/choose_proxy.js
@@ -1,23 +1,44 @@
 // vim: set sw=2 ts=2 softtabstop=2 expandtab :
 
+const cboxGlobal = document.querySelector("#cbox_global");
+const cboxWindow = document.querySelector("#cbox_window");
+const btnOption = document.querySelector("#btn_option");
+
+async function onChangeGlobalProxy() {
+  await browser.runtime.sendMessage({ command: cboxGlobal.checked ? "global_proxy" : "global_direct" });
+  await updateMenuItems();
+}
+
+async function onChangeWindowProxy() {
+  let winInfo = await browser.windows.getCurrent();
+  const windowId = winInfo.id;
+  await browser.runtime.sendMessage({ command: cboxWindow.checked ? "window_proxy" : "window_direct", windowId: windowId });
+  await updateMenuItems(windowId);
+}
+
+async function processOptionClick() {
+  await browser.runtime.openOptionsPage();
+  window.close();
+}
+
 function listenForClicks() {
-  document.addEventListener("click", (e) => {
+  cboxGlobal.addEventListener("change", onChangeGlobalProxy);
+  cboxWindow.addEventListener("change", onChangeWindowProxy);
+  btnOption.addEventListener("click", processOptionClick);
+}
 
-    if (e.target.classList.contains("proxy")) {
-      browser.runtime.sendMessage({command: "proxy"}).then((msg) => {
-        window.close();
-      });
-    }
-    else if (e.target.classList.contains("direct")) {
-      browser.runtime.sendMessage({command: "direct"}).then((msg) => {
-        window.close();
-      });
-    }
-    else if (e.target.classList.contains("option")) {
-      browser.runtime.openOptionsPage().then(() => { window.close(); });
-    }
+async function updateMenuItems(windowId) {
+  if (windowId === undefined) {
+    let winInfo = await browser.windows.getCurrent();
+    windowId = winInfo.id;
+  }
 
-  });
+  let proxyState = await browser.runtime.sendMessage({ command: "get_proxy_state", windowId: windowId });
+  const globalState = (proxyState.global != 0);
+
+  cboxGlobal.checked = globalState;
+  cboxWindow.checked = proxyState.window === undefined ? globalState : (proxyState.window != 0);
 }
 
 listenForClicks();
+updateMenuItems();
